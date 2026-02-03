e49d7f323fcbc3655b0089d02062ba5b00fa4e8d
impradip <pradipyadav2438317@gmail.com>
2026-02-04 00:10:07 +0530

    add code 63

---
+++ 63Static_array.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+//static memory allocation
+// int main(){
+//     int elements;
+//     int arr[5];
+//     //input--> elements
+//     for (int i=0; i<5;i++){
+//         cin>>arr[i];
+//     }
+//     //output-->elements
+//     for(int i=0;i<5;i++){
+//         cout<<arr[i]<<" ";
+//     }
+// }
+
+// static memory allocation-- > another way
