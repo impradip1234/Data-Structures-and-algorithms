d9cc844949edbc440e09cf8f530310c6568a4a86
impradip <pradipyadav2438317@gmail.com>
2026-05-29 09:49:44 +0530

    add code file 187

--- 187Pass_by_reference.cpp
+++ 187Pass_by_reference.cpp
@@ -3,8 +3,13 @@ using namespace std;
 int update(int &a){
     a++;// here the a is pointing to the same variable a ..... so the value is updated directly to a and becomer 56.
 }
+int update2(int*& p){
+    *p=*p+1;
+}
 int main(){
     int a=55;
-    update(a);
+    // update(a);
+    int *p=&a;
+    update2(p);
     cout<<a<<endl;
 }
